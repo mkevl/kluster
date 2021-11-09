efa1d7d4af1206332ede6372397c0e69fe7403cf
Mery Chonishvili <mchon14@freeuni.edu.ge>
2021-11-09 23:21:18 +0400

    Add logo ImageFields to insurance provider

--- insurance/models.py
+++ insurance/models.py
@@ -1,18 +1,19 @@
 import uuid as uuid
 from django.db import models
-from django.db.models import TextField, DecimalField, UUIDField, DateTimeField, URLField
+from django.db.models import TextField, DecimalField, UUIDField, DateTimeField, URLField, CharField, ImageField
 
 
 class InsuranceProvider(models.Model):
     uuid = UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = TextField(null=False)
-    key = TextField(null=False, unique=True)
+    name_en = TextField(blank=True, null=True)
+    key = CharField(null=False, unique=True, max_length=50)
     description = TextField(null=False)
     phone = TextField(blank=True, null=True)
     email = TextField(blank=True, null=True)
     price_per_month = DecimalField(decimal_places=2, max_digits=12)
-    image_url = URLField(blank=True)
-    logo_image_url = URLField(blank=True)
+    provider_image = ImageField(blank=True)
+    provider_logo = ImageField(blank=True)
     price_last_updated_at = DateTimeField(blank=True, auto_now_add=True)
     created_at = DateTimeField(blank=True, auto_now_add=True)
     updated_at = DateTimeField(blank=True, auto_now=True)

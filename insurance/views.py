e28f00e6e75ef535848dbcfcb48dc27babb89c0d
Mery Chonishvili <mchon14@freeuni.edu.ge>
2021-11-10 00:46:21 +0400

    Use query params to filter by insurance and package types

--- insurance/views.py
+++ insurance/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from rest_framework import viewsets
 
 from insurance.models import InsuranceProvider, InsurancePackage
@@ -12,3 +13,17 @@ class InsuranceProviderViewSet(viewsets.ModelViewSet):
 class InsurancePackageViewSet(viewsets.ModelViewSet):
     queryset = InsurancePackage.objects.all()
     serializer_class = InsurancePackageSerializer
+
+    def list(self, request, *args, **kwargs):
+        query_params = request.query_params
+        queries = []
+        if 'insurance_type' in query_params:
+            queries.append(
+                Q(insurance_type=query_params.get('insurance_type')))
+        if 'package_type' in query_params:
+            queries.append(Q(package_type=query_params.get('package_type')))
+
+        if queries:
+            self.queryset = self.queryset.filter(*queries)
+
+        return super(InsurancePackageViewSet, self).list(request, *args, **kwargs)

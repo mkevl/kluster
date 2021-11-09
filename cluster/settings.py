b028e811800a652a6b4feeed4f808f7e965fe50f
Mery Chonishvili <mchon14@freeuni.edu.ge>
2021-11-10 00:38:46 +0400

    Add MEDIA settings

--- cluster/settings.py
+++ cluster/settings.py
@@ -138,6 +138,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_ROOT = env.str('MEDIA_ROOT')
+MEDIA_URL = env.str('MEDIA_URL')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
